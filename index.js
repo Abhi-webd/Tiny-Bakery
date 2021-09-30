92a456b8ff843c15af095b054222f847580eccf8
Abhi-webd <83855664+Abhi-webd@users.noreply.github.com>
2021-09-30 13:10:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var text = ["Cakes", "Ice-Creams", "Chocolates","Cookies","Donuts"];
+var image = ["image/rasmalai cake.jpg",
+"image/rose ice.jpeg",
+"image/gulkand choco.jpeg",
+"image/chocochip cook.jpg",
+"image/choco do.jpeg"];
+
+var counter = 0;
+var elem = document.getElementById("elements");
+var elem2 = document.getElementById("home-image");
+var inst = setInterval(change, 5000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  elem2.setAttribute("src",image[counter])
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+
+}
+
+var date = new Date();
+var year = date.getFullYear();
+var badal = document.getElementById("Copy");
+
+
+function saal(){
+  badal.innerHTML = year;
+}
+
+saal();
+
+
+
